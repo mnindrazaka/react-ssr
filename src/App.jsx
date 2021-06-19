e5c91d6c28e061136b490af6a1c490549fc3061c
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2021-06-19 15:15:41 +0700

    feat: implement client side rendering

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Hello Dani</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+
+const baseConfig = {
+  entry: "./src/index.jsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/i,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
+
+const productionConfig = merge(baseConfig, {
+  mode: "production",
+});
+
+const developmentConfig = merge(baseConfig, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./build",
+  },
+});
+
+module.exports =
+  process.env.WEBPACK_ENV === "production"
+    ? productionConfig
+    : developmentConfig;
